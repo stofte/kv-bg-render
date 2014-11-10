a5f71d9b5a2ea36916e602754154aec7cddcf811
Svend Tofte <stofte@gmail.com>
2014-11-10 23:46:30 +0100

    random polygons

--- bg.js
+++ bg.js
@@ -1,55 +1,88 @@
 (function() {
     'use strict';
 
-    var stageWidth = 500;
-    var stageHeight = 500;
+    var stageWidth = window.innerWidth;
+    var stageHeight = window.innerHeight;
 
+    var polygonLength = 1200;
+    var polygonHeight = 200;
 
-
-    function createGradientTexture() {
+    function createGradientTexture(l,w) {
         var canvas = document.createElement('canvas');
-        canvas.height = stageHeight;
-        canvas.width = stageWidth;
+        canvas.width = polygonLength;
+        canvas.height = polygonHeight;
         var ctx = canvas.getContext('2d');
-        var gradient = ctx.createLinearGradient(0, 0, 350, 350);
-        gradient.addColorStop(0, 'rgba(150,150,150, 1)');
-        gradient.addColorStop(1, 'rgba(50,50,50, 0)');
+        var gradient = ctx.createLinearGradient(0, 0, 0, polygonHeight);
+        gradient.addColorStop(0, 'rgba(100,100,100, 1)');
+        gradient.addColorStop(1, 'rgba(100,100,100, 0)');
         ctx.fillStyle = gradient;
-        ctx.fillRect(0,0,stageWidth,stageHeight);
+        ctx.fillRect(0,0,polygonLength,polygonHeight);
+        // document.body.appendChild(canvas);
         return PIXI.Texture.fromCanvas(canvas);
     }
+
+    function createMask(maxl, maxw) {
+        var w = maxw - Math.random()*maxw*0.5;
+        var l = maxl - Math.random()*maxl*0.3;
+
+        return [ // x,y
+            0,0,
+            l,0,
+            l-w,w,
+            0,w,
+            0,0
+        ];
+    }
+
+    function createSprite() {
+        var sprite = new PIXI.Sprite(texture1);
+        var mask = new PIXI.Graphics();
+        mask.beginFill(0xFF00FF);
+        mask.drawPolygon(createMask(polygonLength, polygonHeight));
+        sprite.mask = mask;
+        return new PIXI.Sprite(sprite.generateTexture());
+    }
  
-    // create an new instance of a pixi stage
-    var stage = new PIXI.Stage(0xFFFFFF);
- 
-    // create a renderer instance.
     var renderer = PIXI.autoDetectRenderer(stageWidth, stageHeight);
- 
-    console.log(renderer);
+    var stage = new PIXI.Stage(0x171717);
+    var rotation= Math.PI * 0.25;
+    var texture1 = createGradientTexture();
+
+
+    var polys = [];
+    for (var i = 0; i < 20; i++) {
+        var polygon = createSprite();
+        polygon.rotation = rotation;
+        polygon.position.set(300+Math.random()*stageWidth, stageHeight*0.8);
+        polygon.anchor.set(1,1);
+        if (Math.random() > 0.5) {
+            polygon.scale.x = -1;
+            polygon.rotation *= -1;
+        }
+        polys.push(polygon);
+        stage.addChild(polygon);
+    }
+
+
+    // var polygon = createPolygon();// new PIXI.Sprite(grad.generateTexture());
+    // polygon.rotation = rotation;
+    // polygon.anchor.set(1, 1);
+    // polygon.position.set(500, 800);
+    // stage.addChild(polygon); 
+
+    // var pol2 = createPolygon();
+    // pol2.rotation = rotation;
+    // pol2.anchor.set(1, 1);
+    // pol2.position.set(520, 800);
+    // stage.addChild(pol2); 
+
+    
+    
     document.body.appendChild(renderer.view);
-
-    var grad = new PIXI.Sprite(createGradientTexture());
-    stage.addChild(grad);
-    grad.position.set(0, 0);
-    console.log(grad)
-
-    var gradMask = new PIXI.Graphics();
-    stage.addChild(gradMask);
-    gradMask.beginFill(0);
-    gradMask.position.set(stageWidth/2, stageHeight/2);
-    gradMask.pivot.set(stageWidth/2, stageHeight/2);
-    gradMask.drawRect(10, 10, stageWidth-20, stageHeight-20);
-    grad.mask = gradMask;
- 
     requestAnimFrame(animate);
  
     function animate() {
         requestAnimFrame(animate);
-
-        gradMask.rotation += 0.01;
-        
         renderer.render(stage);
     }
-
-    
 })();
