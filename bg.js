a1ab1e2502ed5f4fe4a189ae0812ce4185ac75ba
Svend Tofte <stofte@gmail.com>
2014-11-11 01:13:56 +0100

    sliding polygons

--- bg.js
+++ bg.js
@@ -1,11 +1,10 @@
 (function() {
     'use strict';
-
-    var stageWidth = window.innerWidth;
-    var stageHeight = window.innerHeight;
-
-    var polygonLength = 1200;
-    var polygonHeight = 200;
+    
+    // https://gist.github.com/gre/1650294
+    function easeOutCubic(t) { 
+        return (--t)*t*t+1; 
+    }
 
     function createGradientTexture(l,w) {
         var canvas = document.createElement('canvas');
@@ -17,7 +16,6 @@
         gradient.addColorStop(1, 'rgba(100,100,100, 0)');
         ctx.fillStyle = gradient;
         ctx.fillRect(0,0,polygonLength,polygonHeight);
-        // document.body.appendChild(canvas);
         return PIXI.Texture.fromCanvas(canvas);
     }
 
@@ -42,47 +40,51 @@
         sprite.mask = mask;
         return new PIXI.Sprite(sprite.generateTexture());
     }
- 
+    
+    var stageWidth = window.innerWidth;
+    var stageHeight = window.innerHeight;
+
+    var polygonLength = 1200;
+    var polygonHeight = 240;
+
     var renderer = PIXI.autoDetectRenderer(stageWidth, stageHeight);
     var stage = new PIXI.Stage(0x171717);
     var rotation= Math.PI * 0.25;
     var texture1 = createGradientTexture();
-
+    var polygonShift = stageHeight*0.8;
 
     var polys = [];
-    for (var i = 0; i < 20; i++) {
+    for (var i = 0; i < 15; i++) {
         var polygon = createSprite();
         polygon.rotation = rotation;
-        polygon.position.set(300+Math.random()*stageWidth, stageHeight*0.8);
+        // fix this shit
+        var finalx = Math.random()*stageWidth;
+        polygon.position.__finalx = finalx;
+        polygon.position.set(finalx - polygonShift, 0);
         polygon.anchor.set(1,1);
         if (Math.random() > 0.5) {
             polygon.scale.x = -1;
+            polygon.position.x = finalx + polygonShift;
             polygon.rotation *= -1;
         }
         polys.push(polygon);
         stage.addChild(polygon);
     }
-
-
-    // var polygon = createPolygon();// new PIXI.Sprite(grad.generateTexture());
-    // polygon.rotation = rotation;
-    // polygon.anchor.set(1, 1);
-    // polygon.position.set(500, 800);
-    // stage.addChild(polygon); 
-
-    // var pol2 = createPolygon();
-    // pol2.rotation = rotation;
-    // pol2.anchor.set(1, 1);
-    // pol2.position.set(520, 800);
-    // stage.addChild(pol2); 
-
-    
     
     document.body.appendChild(renderer.view);
     requestAnimFrame(animate);
- 
+
+    var duration = 2500;
+    var start_ts = new Date().getTime();
     function animate() {
+        var delta = (new Date().getTime() - start_ts)/duration;
+        if (delta > 1) return;
         requestAnimFrame(animate);
+        var shift = easeOutCubic(delta)*polygonShift;
+        polys.forEach(function(p) {
+            p.position.x = p.position.__finalx + (p.scale.x != -1?1:-1)*shift;
+            p.position.y = shift;
+        });
         renderer.render(stage);
     }
 })();
